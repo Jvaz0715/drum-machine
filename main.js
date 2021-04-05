b2035d53112c96ba98e4c60af4db5393b1a7c55a
Jvaz0715 <James.Vazquez@codeimmersives.com>
2021-04-05 14:25:04 -0400

    Completes 2C. Adds functionality that will play the instrument when checked

--- main.js
+++ main.js
@@ -3,6 +3,11 @@ const tick = new Audio('sounds/tick.mp3');
 // 1A-1 Setup 'tock' sound
 const tock = new Audio('sounds/tock.mp3');
 
+// 2C load the other instruments, then, update the update() function to play if that instruments checkbox is checked
+const kickDrum = new Audio('sounds/kick-drum.mp3');
+const hiHat = new Audio('sounds/hi-hat.mp3');
+const snareDrum = new Audio('sounds/snare-drum.mp3');
+
 //1A-2 create global variable to keep count;
 let metronomeCount = 0;
 
@@ -28,6 +33,11 @@ function update() {
     // 2B query the metronome checkbox to see if it is checked
     const metronomeCheck = document.querySelector("#metronome-checkbox");
 
+    // 2C query each instruments checkbox to see if it is checked
+    const kickDrumChecked = document.querySelector("#kick-drum");
+    const hiHatChecked = document.querySelector("#hi-hat");
+    const snareDrumChecked = document.querySelector("#snare-drum");
+
     // 2B see if metronome checkbox is checked
     if (metronomeCheck.checked) {
          // Play the 'tick' sound
@@ -39,8 +49,19 @@ function update() {
         if (metronomeCount % 4 === 0) {
             tock.play();
         }
-    } else {
-        
+    }
+
+    // 2C see if each other instrument is checked
+    if (kickDrumChecked.checked) {
+        kickDrum.play();
+    }
+
+    if (hiHatChecked.checked) {
+        hiHat.play();
+    }
+
+    if (snareDrumChecked.checked) {
+        snareDrum.play();
     }
 }
 
